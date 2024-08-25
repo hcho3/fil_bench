1bd5c1a0f84a90d6bf5a1c98357ddba785de5aa9
Hyunsu Cho <phcho@nvidia.com>
2024-08-24 17:46:30 -0700

    Print out string rep of enums

--- src/launch_config.cpp
+++ src/launch_config.cpp
@@ -5,8 +5,39 @@
 namespace fil_bench {
 
 std::ostream& operator<<(std::ostream& os, launch_config_t const& config) {
-  os << "{chunk_size=" << config.chunk_size << ", algo_type=" << config.algo_type
-     << ", storage_type=" << config.storage_type << ", layout=";
+  os << "{chunk_size=" << config.chunk_size << ", algo_type=";
+  switch (config.algo_type) {
+  case ML::fil::algo_t::ALGO_AUTO:
+    os << "ML::fil::algo_t::ALGO_AUTO, ";
+    break;
+  case ML::fil::algo_t::NAIVE:
+    os << "ML::fil::algo_t::NAIVE, ";
+    break;
+  case ML::fil::algo_t::TREE_REORG:
+    os << "ML::fil::algo_t::TREE_REORG, ";
+    break;
+  case ML::fil::algo_t::BATCH_TREE_REORG:
+    os << "ML::fil::algo_t::BATCH_TREE_REORG, ";
+    break;
+  }
+
+  os << "storage_type=";
+  switch (config.storage_type) {
+  case ML::fil::storage_type_t::AUTO:
+    os << "ML::fil::storage_type_t::AUTO, ";
+    break;
+  case ML::fil::storage_type_t::DENSE:
+    os << "ML::fil::storage_type_t::DENSE, ";
+    break;
+  case ML::fil::storage_type_t::SPARSE:
+    os << "ML::fil::storage_type_t::SPARSE, ";
+    break;
+  case ML::fil::storage_type_t::SPARSE8:
+    os << "ML::fil::storage_type_t::SPARSE8, ";
+    break;
+  }
+
+  os << "layout=";
   switch (config.layout) {
   case ML::experimental::fil::tree_layout::depth_first:
     os << "ML::experimental::fil::tree_layout::depth_first}";
